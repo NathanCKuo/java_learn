6999790fe41fbad0f2d031aee27fa331fa6b3245
CK-PC\Natha <nathanckuo@gmail.com>
2017-11-01 14:47:07 -0400

    Java programs

---
+++ CupToPint.java
@@ -0,0 +1,10 @@
+class CupToPint{
+	public static void main(String args[]){
+          double cups;
+	  double pints;
+	  cups = 1000000;
+	  pints = cups/2;
+	  System.out.println(cups + "cups is " + pints + "pints");
+	}
+}
+
---
+++ Example2.java
@@ -0,0 +1,20 @@
+/*   
+   This demonstrates a variable.  
+ 
+   Call this file Example2.java.  
+*/   
+class Example2 {   
+  public static void main(String args[]) {   
+    int var1; // this declares a variable  
+    int var2; // this declares another variable  
+  
+    var1 = 1024; // this assigns 1024 to var1  
+  
+    System.out.println("variable1 contains " + var1);   
+  
+    var2 = var1 / 2;  
+  
+    System.out.print("variable2 contains var1 / 2: ");  
+    System.out.println(var2);  
+  }   
+}
