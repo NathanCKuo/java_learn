ba39bc6409176ea68313befbb7527e5cfa8820d9
CK-PC\Natha <nathanckuo@gmail.com>
2017-11-08 10:11:16 -0500

    no message

---
+++ Ex2.java
@@ -0,0 +1,12 @@
+class Ex2 {
+	public static void main(String args[]) {
+	  int num1;
+	  int num2;
+	  boolean value;
+	  num1 = 245;
+	  num2 = 359;
+	  value = false;
+	  if(num2>num1) System.out.println("num1 is less than num2");
+	  if(value) System.out.println("value is false");
+	}
+}
